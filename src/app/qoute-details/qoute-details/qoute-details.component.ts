4f660f0537293313aed6e2769f9ab8e5ee62aafe
ZashyAris <sharonatieno080@gmail.com>
2020-01-22 17:30:31 +0300

    created a qoute-details.component.ts file and added imports and functions to the QouteDetailsComponent class

---
+++ src/app/qoute-details/qoute-details/qoute-details.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+import { Qoute } from "../qoutes";
+@Component({
+  selector: "app-qoute-details",
+  templateUrl: "./qoute-details.component.html",
+  styleUrls: ["./qoute-details.component.css"]
+})
+export class QouteDetailsComponent implements OnInit {
+
+  @Input() qoutes: Qoute;
+ 
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  qouteDelete(complete: boolean) {
+    this.isComplete.emit(complete);
+  }
+
+  numberoflikes : number=0
+  numberofdislikes : number=0
+     likebuttonclick(){
+     this.numberoflikes++;
+}
+  dislikebuttonclick(){
+  this.numberofdislikes++;
+}
+
+  constructor() {
+  }
+
+  ngOnInit() {}
+}
